8175c4244a0fa9b77e525079c75c7100260e14e3
Mikhail <anohin04@mail.ru>
2022-04-15 21:25:37 +0500

    Creating object

---
+++ Spring/src/main/java/com/home/TestBean.java
@@ -0,0 +1,17 @@
+package com.home;
+
+public class TestBean {
+    private String name;
+
+    TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
