9a57fc600641774cf37f98087e1331e66b30cc01
Mikhail <anohin04@mail.ru>
2022-04-22 01:46:37 +0500

    MVC java

---
+++ SpringMVCJava/src/main/java/com/home/HelloController.java
@@ -0,0 +1,12 @@
+package com.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "helloWorld";
+    }
+}
