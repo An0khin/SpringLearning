36dd8ae7306e81b26e278f8a2b72ebb9fb5fe071
Mikhail <anohin04@mail.ru>
2022-04-25 02:18:18 +0500

    Requests MVC

--- SpringRepeatingMVC2/src/main/java/com/home/controllers/firstController.java
+++ SpringRepeatingMVC2/src/main/java/com/home/controllers/firstController.java
@@ -1,13 +1,26 @@
 package com.home.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class firstController {
     @GetMapping("first")
-    public String first() {
+    public String first(HttpServletRequest request, Model model) { //always return "first"
+        //System.out.println(request.getParameter("name"));
+        model.addAttribute("mes", "Name is " + request.getParameter("name")); //model to html page
         return "first";
     }
+
+//    @GetMapping("first")
+//    public String first(@RequestParam("name") String param) { //return "first" if url has "name" parameter
+//        System.out.println(param);
+//        return "first";
+//    }
 
 }
